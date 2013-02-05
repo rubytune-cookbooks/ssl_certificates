0fddd2722216687e545ec3cafb0e82be32aae17f
Joshua Sierles <joshua@diluvia.net>
2013-02-05 11:52:03 +0100

    cleanup

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,4 @@
-maintainer        "37signals"
-maintainer_email  "sysadmins@37signals.com"
+maintainer        "Joshua Sierles"
+maintainer_email  "joshua@rubytune.com"
 description       "Configures SSL certificate"
-version           "0.1"
+version           "0.2"
